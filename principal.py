bbfd8cbbe1ac4bbc7159c39bc9d5a39b71f83812
EDISON ROSALES <edisonrosalescampos@gmail.com>
2023-06-24 22:27:20 -0400

    versión 1.0 terminada

--- principal.py
+++ principal.py
@@ -7,17 +7,15 @@ class Calculadora:
     #self.ventana.geometry("392x600")
     
     # CONFIGURAMOS LA PANTALLA
-    self.pantalla = Entry(self.ventana, font=("Helvetica", 15))
-    self.pantalla.grid(row=0, column=0, columnspan=4, sticky="WE", ipady=20)
+    self.pantalla = Text(self.ventana, width=0, height=3, font=("Helvetica", 15))
+    self.pantalla.grid(row=0, column=0, columnspan=4, sticky="ew")
 
     # CREAMOS UNA VARIABLE OPERACIÓN VACÍA
     self.operacion = "" 
 
     # CREAMOS LA BOTONERA DE LA CALCULADORA
     botonera = []
-    botonera.append(self.crearBoton("C"))
-    botonera.append(self.crearBoton("CE"))
-    botonera.append(self.crearBoton("R"))
+    botonera.append(self.crearBoton("BORRAR"))
     botonera.append(self.crearBoton(u"\u232B"))
     botonera.append(self.crearBoton("7"))
     botonera.append(self.crearBoton("8"))
@@ -26,7 +24,7 @@ class Calculadora:
     botonera.append(self.crearBoton("4"))
     botonera.append(self.crearBoton("5"))
     botonera.append(self.crearBoton("6"))
-    botonera.append(self.crearBoton("x"))
+    botonera.append(self.crearBoton("*"))
     botonera.append(self.crearBoton("1"))
     botonera.append(self.crearBoton("2"))
     botonera.append(self.crearBoton("3"))
@@ -35,23 +33,67 @@ class Calculadora:
     botonera.append(self.crearBoton("0"))
     botonera.append(self.crearBoton("="))
     botonera.append(self.crearBoton("+"))
-    #  botonera.append(self.crearBoton("=", 40))
 
-    fila = 1
-    columna = 0    
-    for indice, boton in enumerate(botonera):
-      if indice != 0 and indice % 4 == 0:
-        fila = fila + 1
-        columna = 0
+    botonera[0].grid(row=1, column=0, columnspan=2, sticky="ew")
+    botonera[1].grid(row=1, column=2, columnspan=2, sticky="ew")
 
-      boton.grid(row=fila, column=columna)
-      
-      columna = columna + 1
+    contador = 2  
+    for fila in range(2, 6):
+      for columna in range(4):
+        botonera[contador].grid(row=fila, column=columna)
+
+        contador += 1
 
     # BUCLE INFINITO
     self.ventana.mainloop()
 
-  def crearBoton(self, texto, ancho=8, alto=4):
-    return Button(self.ventana, text=texto, width=ancho, height=alto, font=("Helvetica", 10))
+  def crearBoton(self, valor, ancho=8, alto=4):
+    return Button(self.ventana, text=valor, width=ancho, height=alto, font=("Helvetica", 10), command=lambda:self.capturarClic(valor))
+  
+  def capturarClic(self, tecla):
+    # OPCIÓN IGUAL
+    if tecla == "=" and self.operacion != "":
+      resultado = eval(self.operacion);
+      self.limpiarPantalla()
+      self.mostrarPantalla(resultado)
+
+    # OPCIÓN LIMPIAR PANTALLA
+    elif tecla == "BORRAR":
+      self.operacion = ""
+      self.limpiarPantalla()
+
+    # OPCIÓN BORRAR ÚLTIMO CARACTER
+    elif tecla == u"\u232B":
+      self.borrarPantalla()
+
+    # TODAS LAS DEMAS TECLAS
+    else:
+      self.operacion += tecla
+      self.mostrarPantalla(tecla)
+
+  def habilitarPantalla(self):
+    self.pantalla.configure(state="normal")
+
+  def deshabilitarPantalla(self):
+    self.pantalla.configure(state="disabled")
+
+  def borrarPantalla(self):
+    self.habilitarPantalla()
+    texto = self.pantalla.get(1.0, END)
+    texto = texto[:-2]
+    self.pantalla.delete(1.0, END)
+    self.pantalla.insert(END, texto)
+    self.deshabilitarPantalla()
+
+  def limpiarPantalla(self):
+    self.habilitarPantalla()
+    self.pantalla.delete(1.0, END)
+    self.deshabilitarPantalla()
+
+  def mostrarPantalla(self, texto):
+    self.habilitarPantalla()
+    self.pantalla.insert(END, texto)
+    self.deshabilitarPantalla()
+
 
 calculadora = Calculadora()
